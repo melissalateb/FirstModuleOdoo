f1ec8167adca2593374d0c9353f1a80993503744
midadey <60318243+midadey@users.noreply.github.com>
2023-04-13 09:06:56 +0200

    Add files via upload

---
+++ odoo-compose/addons/Melissa/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name' : 'Melissa',
+    'summary' : '',
+
+    'description' : 'hello world',
+    'website':'https://yourcompany.com',
+    'version': '1.0',
+    
+
+    'author' :"Melissa",
+
+}
